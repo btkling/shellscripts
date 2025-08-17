8f1bace00445f7947e0f6b1f9d45006b0fc86401
Ben Kling <kling.ben@gmail.com>
2025-08-17 15:59:48 -0400

    add backup_nvim_cfg.sh

---
+++ backup_nvim_cfg.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/sh
+
+mv ~/.config/nvim ~/.config/nvim.backup
+mv ~/.local/share/nvim ~/.local/share/nvim.backup
+mv ~/.local/state/nvim ~/.local/state/nvim.backup
+mv ~/.cache/nvim ~/.cache/nvim.backup
